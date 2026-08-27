b4d35007b8537839a6a60c990c9f157bf1013ea8
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-08-27 09:12:07 -0300

    feat: refactor form submission test to improve readability and add checkbox handling

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -1,28 +1,47 @@
-describe('Formulario', ()=>{
+describe("Formulario", () => {
+  it("teste", () => {
+    cy.login("4DM@gmail.com", "4DM");
+    cy.contains("button", "Formulários").should("be.visible").click("");
+    cy.contains("h1", "Consultoria").should("be.visible");
+    cy.get("#name").type("Luiz Miguel");
+    cy.get("#email").type("Luiz@gmail.com");
+    cy.get("#phone").type("11915882337");
+    cy.get("#consultancyType").select("In Company");
+    cy.contains("label", "Pessoa Jurídica")
+      .click()
+      .find("input[type=radio]")
+      .should("be.checked");
+    cy.contains("label", "Pessoa Física")
+      .find("input[type=radio]")
+      .should("be.not.checked");
+    const sociais = [
+      "Instagram",
+      "LinkedIn",
+      "Udemy",
+      "YouTube",
+      "Indicação de Amigo",
+    ];
+    sociais.forEach((sociais) => {
+      cy.contains("label", sociais)
+        .find("input[type=checkbox]")
+        .click()
+        .should("be.checked");
+    });
 
-	it('teste', ()=> {
- 
-        cy.login('4DM@gmail.com', '4DM')
-        cy.contains('button','Formulários')
-        .should('be.visible')
-        .click('')
-        cy.contains('h1','Consultoria')
-        .should('be.visible')
-        cy.get('#name').type("Luiz Miguel")
-        cy.get('#email').type("Luiz@gmail.com")
-        cy.get('#phone').type("11915882337")
-        cy.get('#consultancyType').select("In Company")
-        cy.contains('label', 'Pessoa Jurídica').click().find('input[type=radio]').should('be.checked')
-        cy.contains('label','Pessoa Física').find('input[type=radio]').should('be.not.checked')
-        cy.contains('label', 'Instagram').click()
-        cy.contains('label', 'Udemy').click()
-        cy.contains('label', 'LinkedIn').click()
-        cy.get('#document').type("10033051488")
-        cy.get('#details').type("Muito foda")
-        cy.get('#technologies').type("Java")
-        cy.contains('label', 'Li e aceito os termos de uso *').click()
-        cy.contains('button', 'Enviar Documento').click()
+    cy.get("#document").type("10033051488");
+    cy.get("#details").type("Muito foda");
 
-
+    const tecnologia = [
+        "Java ",
+        "Javascript ",
+        "Git "
+    ]
+    tecnologia.forEach((tecnologia)=>{
+        cy.get("#technologies").type(tecnologia + '{enter}')
     })
-})
+
+   // cy.get("#technologies").type("Java");
+    cy.contains("label", "Li e aceito os termos de uso *").click();
+    cy.contains("button", "Enviar formulário").click();
+  });
+});
